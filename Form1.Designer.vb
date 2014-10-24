ce6b3cf7866273beb0f7b11a7603878c67e5d64b
katyeneza <yenezak@pagosa.k12.co.us>
2014-10-23 20:38:13 -0600

    start

---
+++ Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.txtTest1 = New System.Windows.Forms.TextBox()
+        Me.txtTest2 = New System.Windows.Forms.TextBox()
+        Me.txtTest3 = New System.Windows.Forms.TextBox()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Location = New System.Drawing.Point(37, 22)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(204, 13)
+        Me.lblInstructions.TabIndex = 0
+        Me.lblInstructions.Text = "Put in three numbers to find their average."
+        '
+        'txtTest1
+        '
+        Me.txtTest1.Location = New System.Drawing.Point(38, 65)
+        Me.txtTest1.Name = "txtTest1"
+        Me.txtTest1.Size = New System.Drawing.Size(68, 20)
+        Me.txtTest1.TabIndex = 1
+        '
+        'txtTest2
+        '
+        Me.txtTest2.Location = New System.Drawing.Point(39, 97)
+        Me.txtTest2.Name = "txtTest2"
+        Me.txtTest2.Size = New System.Drawing.Size(66, 20)
+        Me.txtTest2.TabIndex = 2
+        '
+        'txtTest3
+        '
+        Me.txtTest3.Location = New System.Drawing.Point(40, 136)
+        Me.txtTest3.Name = "txtTest3"
+        Me.txtTest3.Size = New System.Drawing.Size(66, 20)
+        Me.txtTest3.TabIndex = 3
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(38, 230)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(9, 13)
+        Me.lblAnswer.TabIndex = 4
+        Me.lblAnswer.Text = "l"
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(38, 162)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(95, 28)
+        Me.btnCheck.TabIndex = 5
+        Me.btnCheck.Text = "Find average"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.txtTest3)
+        Me.Controls.Add(Me.txtTest2)
+        Me.Controls.Add(Me.txtTest1)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblInstructions As System.Windows.Forms.Label
+    Friend WithEvents txtTest1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtTest3 As System.Windows.Forms.TextBox
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents btnCheck As System.Windows.Forms.Button
+
+End Class
