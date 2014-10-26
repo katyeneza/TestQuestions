5cfa61b8d0c20a9bcf9acea725f5d9fd946abda9
katyeneza <yenezak@pagosa.k12.co.us>
2014-10-26 00:55:04 -0600

    question15

--- Question15/Question15/Form1.Designer.vb
+++ Question15/Question15/Form1.Designer.vb
@@ -22,17 +22,87 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnAnswer = New System.Windows.Forms.Button()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.txtMonth = New System.Windows.Forms.TextBox()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'btnAnswer
+        '
+        Me.btnAnswer.Location = New System.Drawing.Point(83, 130)
+        Me.btnAnswer.Name = "btnAnswer"
+        Me.btnAnswer.Size = New System.Drawing.Size(142, 65)
+        Me.btnAnswer.TabIndex = 0
+        Me.btnAnswer.Text = "Answer"
+        Me.btnAnswer.UseVisualStyleBackColor = True
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(62, 230)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(9, 13)
+        Me.lblAnswer.TabIndex = 1
+        Me.lblAnswer.Text = "l"
+        '
+        'txtMonth
+        '
+        Me.txtMonth.Location = New System.Drawing.Point(64, 35)
+        Me.txtMonth.Name = "txtMonth"
+        Me.txtMonth.Size = New System.Drawing.Size(84, 20)
+        Me.txtMonth.TabIndex = 2
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(65, 70)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(83, 20)
+        Me.txtYear.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Month"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Year"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtMonth)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.btnAnswer)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents btnAnswer As System.Windows.Forms.Button
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents txtMonth As System.Windows.Forms.TextBox
+    Friend WithEvents txtYear As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- Question15/Question15/Form1.vb
+++ Question15/Question15/Form1.vb
@@ -1,4 +1,42 @@
 ﻿Public Class Form1
 
-    
+
+    Private Sub btnAnswer_Click(sender As Object, e As EventArgs) Handles btnAnswer.Click
+        Dim strMonth As String = Me.txtMonth.Text
+        Dim intYear As Integer = Me.txtYear.Text
+
+        Dim strAnswer1 As String = "JANUARY"
+        Dim strAnswer2 As String = "FEBRUARY"
+        Dim strAnswer3 As String = "MARCH"
+        Dim strAnswer4 As String = "APRIL"
+        Dim strAnswer5 As String = "MAY"
+        Dim strAnswer6 As String = "JUNE"
+        Dim strAnswer7 As String = "JULY"
+        Dim strAnswer8 As String = "AUGUST"
+        Dim strAnswer9 As String = "SEPTEMBER"
+        Dim strAnswer10 As String = "OCTOBER"
+        Dim strAnswer11 As String = "NOVEMBER"
+        Dim strAnswer12 As String = "DECEMBER"
+
+        Select Case Me.txtMonth.Text
+            Case strAnswer1, strAnswer3, strAnswer5, strAnswer7, strAnswer8, strAnswer10, strAnswer12
+                Me.lblAnswer.Text = "There are 31 days in this month."
+            Case strAnswer4, strAnswer6, strAnswer9, strAnswer11
+                Me.lblAnswer.Text = "There are 30 days in this month."
+            Case strAnswer2
+                If intYear Mod 4 <> 0 Then
+                    Me.lblAnswer.Text = "There are 28 days in this month."
+                ElseIf intYear Mod 400 = 0 Then
+                    Me.lblAnswer.Text = "There are 29 days in this month."
+                ElseIf intYear Mod 100 = 0 Then
+                    Me.lblAnswer.Text = "There are 28 days in this month."
+                Else
+                    lblAnswer.Text = "There are 29 days in this month."
+                End If
+            Case Else
+        End Select
+
+    End Sub
+
 End Class
+
